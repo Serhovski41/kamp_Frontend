5edc67b6e7b80075d950ce614eb97edcc7658d4c
serhovski <sero41fb@outlook.com>
2023-02-11 18:18:06 +0300

    11 February Commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,6 +5,8 @@ import {
   Validators,
   FormBuilder,
 } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -12,8 +14,10 @@ import {
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  
   loginForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder:FormBuilder,
+    private authService:AuthService, private toastrService:ToastrService) {}
 
   ngOnInit(): void {
     this.createLoginForm();
@@ -29,6 +33,12 @@ export class LoginComponent implements OnInit {
   login(){
     if(this.loginForm.valid){
       console.log(this.loginForm.value);
+      let loginModel = Object.assign({},this.loginForm.value)
+
+      this.authService.login(loginModel).subscribe(response=>{
+        this.toastrService.info(response.message)
+        localStorage.setItem("token",response.data.token)
+      })
     }
   }
 
