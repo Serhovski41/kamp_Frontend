0e238631d349444144edd43251e411b477927121
serhovski <sero41fb@outlook.com>
2023-02-16 01:56:02 +0300

    16 January Last Commit

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators} from '@angular/forms';
+import {FormGroup,FormBuilder, FormControl, Validators} from "@angular/forms"
 import { ToastrService } from 'ngx-toastr';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -11,22 +11,21 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductAddComponent implements OnInit {
 
-  productAddForm: FormGroup;
-  constructor(private formBuilder:FormBuilder,
-    private productService:ProductService,
-    private toastrService:ToastrService){ }
+  productAddForm : FormGroup;
+  constructor(private formBuilder:FormBuilder, 
+    private productService:ProductService, private toastrService:ToastrService) { }
 
   ngOnInit(): void {
     this.createProductAddForm();
-}
+  }
 
   createProductAddForm(){
-    this.productAddForm = this.formBuilder.group({
-      productName:["",Validators.required],
-      unitPrice: ["", Validators.required],
-      unitsInStock:["",Validators.required],
-      categoryId:["",Validators.required]
-    })
+     this.productAddForm = this.formBuilder.group({
+       productName:["",Validators.required],
+       unitPrice: ["",Validators.required],
+       unitsInStock:["", Validators.required],
+       categoryId:["",Validators.required]
+     })
   }
 
   add(){
